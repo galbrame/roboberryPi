bc09fbc27eb8f7e02725393d1b7da08a977b3a04
galbrame <meg_galbraith@hotmail.com>
2022-11-28 08:58:27 -0600

    Add Python Control Scripts
    Added the Python versions of the .cgi bash scripts. The website will
    allow users to toggle between the two modes and log any time diffs
    in responsiveness.

---
+++ py-bin/right.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+
+import RPi.GPIO as GPIO
+import projectConsts as PROJ
+
+GPIO.setmode(GPIO.BCM)
+
+# shouldn't have to do this because set at start up
+#GPIO.setup(PROJ.LEFT_FWD, GPIO.OUT)
+#GPIO.setup(PROJ.LEFT_REV, GPIO.OUT)
+#GPIO.setup(PROJ.RIGHT_FWD, GPIO.OUT)
+#GPIO.setup(PROJ.RIGHT_REV, GPIO.OUT)
+
+GPIO.output(PROJ.LEFT_FWD, 1)
+GPIO.output(PROJ.LEFT_REV, 0)
+GPIO.output(PROJ.RIGHT_FWD, 0)
+GPIO.output(PROJ.RIGHT_REV, 1)
