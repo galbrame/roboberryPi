62b4c4687c3ac5db71cb8d9d26aab7725f665c5a
Megan Galbraith <meg_galbraith@hotmail.com>
2022-12-08 22:20:03 -0600

    Restructure asset directory
    Moved all of the .png files the the assets folder into a new /icons subfolder.
    Added an image attribution file as well.

---
+++ assets/icons/imageAttributions.md
@@ -0,0 +1,25 @@
+# Flaticon
+
+### raspberry-pi.png:
+[Raspberry pi icons created by Freepik - Flaticon](https://www.flaticon.com/free-icons/raspberry-pi)
+
+
+# icons8
+
+### icons8-github-64.png
+[Github icon](https://icons8.com/icon/3tC9EQumUAuq/github)
+
+### icons8-forward-64.png
+["Sort up" icon](https://icons8.com/icon/71055/sort-up)
+
+### icons8-left-64.png
+["Sort left" icon](https://icons8.com/icon/52575/sort-left)
+
+### icons8-right-64.png
+["Play" icon](https://icons8.com/icon/44036/play)
+
+### icons8-reverse-64.png
+["Sort down" icon](https://icons8.com/icon/49477/sort-down)
+
+### icons8-stop-64.png
+["Stop" icon](https://icons8.com/icon/48210/stop)
--- assets/roboScripts.js
+++ assets/roboScripts.js
@@ -5,3 +5,92 @@
 *
 *******************************************/
 
+var WEB_SERVER = "robopi.local:5000" //home LAN
+//var WEB_SERVER = "140.193.105.207:5000" //um-secure IP
+var myRequest; //XHR request
+
+
+/******************************************
+* getRadioBtnValue
+*
+* DESCRIPTION: Gets the currently checked radio button value
+* 
+* RETURNS:
+*       val: the value stored in the checked radio button (ie, speed)
+******************************************/
+function getRadioBtnValue() {
+    let radioGrp = document.getElementsByName("speed");
+    let val = 0;
+
+    for (let i = 0; i < radioGrp.length; i++) {
+        if (radioGrp[i].checked) {
+            val = Number(radioGrp[i].value);
+        }
+    }
+
+    return val;
+}
+
+
+/******************************************
+* POSTVerificationLoadEvent
+*
+* DESCRIPTION: Used to log errors, as valid responses should be real-time
+*              observeable, as in the roboberry will not move/speed up as
+*              expected.
+******************************************/
+function POSTVerificationLoadEvent() {
+    if (myRequest.status != 200) {
+        console.log("A error occured:");
+        console.log(myRequest.responseText);
+    }
+}
+
+
+/******************************************
+* moveCar
+*
+* DESCRIPTION: Tell the roboberry to move or change direction
+* 
+* PARAMETERS:
+*       direction: the direction to move the roboberry
+******************************************/
+function moveCar(direction) {
+    let speed = getRadioBtnValue();
+    let reqBody = "direction=" + direction + "\nspeed="+ speed;
+    
+    myRequest = new XMLHttpRequest();
+    myRequest.addEventListener("load", POSTVerificationLoadEvent);
+    myRequest.open("POST", WEB_SERVER + "/api/move");
+    myRequest.send(JSON.stringify(reqBody));
+}
+
+
+/******************************************
+* stopCar
+*
+* DESCRIPTION: Sends a stop message to the roboberry
+******************************************/
+function stopCar() {
+    myRequest = new XMLHttpRequest();
+    myRequest.addEventListener("load", POSTVerificationLoadEvent);
+    myRequest.open("POST", WEB_SERVER + "/api/stop");
+    myRequest.send();
+}
+
+
+/******************************************
+* changeSpeed
+*
+* DESCRIPTION: Collects the value of the speedometer radio buttons and updates
+*              the roboberry's speed accordingly
+******************************************/
+function changeSpeed() {
+    // let speed = getRadioBtnValue();
+    // let reqBody = "speed="+ speed;
+
+    // myRequest = new XMLHttpRequest();
+    // myRequest.addEventListener("load", POSTVerificationLoadEvent);
+    // myRequest.open("POST", WEB_SERVER + "/api/stop");
+    // myRequest.send(JSON.stringify(reqBody));
+}
